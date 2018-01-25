e0688ccc3cf460747e7e62aa233f82f256010392
Ying-Hua Hsieh <procandi@gmail.com>
2018-01-25 16:49:34 +0800

    for test.

--- config/environments.rb
+++ config/environments.rb
@@ -1,12 +1,5 @@
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+ ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
+
 end
