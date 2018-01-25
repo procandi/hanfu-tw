50c2fb99621eeab662e3e9bea032193e893f02ba
Ying-Hua Hsieh <procandi@gmail.com>
2018-01-25 16:17:58 +0800

    fix bug.
    fix bug.

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@
 
 #匯入模組
 require 'sinatra'
-#require 'sinatra/base'
 require 'sinatra/activerecord'
 require './models/model'
 require './config/environments'
@@ -15,20 +14,15 @@ class Vote < ActiveRecord::Base
 end
 
 
-class App < Sinatra::Base
-	#處理行事曆
-	get '/calendar' do
-		erb :Calendar
-	end
+#處理行事曆
+get '/calendar' do
+	erb :Calendar
+end
 
-	#處理投票
-	get '/vote' do
-		@votes = Vote.all #find(params[:id])
-		@vote=@votes.first
+#處理投票
+get '/vote' do
+	@votes = Vote.all #find(params[:id])
+	@vote=@votes.first
 
-		erb :Vote
-	end
-
-	#run sinatra server when this site is unstart
-	run! if app_file == $0
+	erb :Vote
 end
