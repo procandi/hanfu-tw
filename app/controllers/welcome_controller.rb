77c7d4703058dc324faf343564e4babc5a334b1e
Ying-Hua Hsieh <procandi@gmail.com>
2018-02-03 12:24:29 +0800

    add vote.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,44 @@
 class WelcomeController < ApplicationController
+
+	
+	def index
+	end
+
+
+	def result
+		email=params[:email]
+
+		vote=Vote.all.where("email='#{email}' and flag=''")
+		if vote==nil
+			@result='您的Email已領過票，或Email錯誤或未登錄'
+		else
+
+			if email.length()>5
+				temp=email[0..4].to_s()
+			else
+				temp=email.to_s()
+			end
+			
+			serial=''
+			temp.each_byte(){|a|
+				serial+=(a.to_i()*860220).to_s()
+			}
+
+			i=0
+			@result=''
+			serial.each_char(){|c|
+				@result+=c
+				if(i%2==1 && i+1!=serial.length)
+					@result+='-'
+				end
+				i+=1
+			}
+
+			@result='您的投票序號為：'+@result
+		end
+	end
+
+
+	def check
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'result' => 'welcome#result'
+  post 'result' => 'welcome#result'
+  get 'check' => 'welcome#check'
+  post 'check' => 'welcome#check'
+
   resources :votes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
