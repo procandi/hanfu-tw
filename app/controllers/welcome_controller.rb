eadc897fe9f2c43374d2bd70252abf57ce8d3947
Ying-Hua Hsieh <procandi@gmail.com>
2018-02-03 17:12:34 +0800

    bugfix.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,10 +8,10 @@ class WelcomeController < ApplicationController
 	def result
 		email=params[:email]
 
-		#vote=Vote.all.where("email='#{email}' and flag=''")
-		#if vote==nil
-		#	@result='您的Email已領過票，或Email錯誤或未登錄'
-		#else
+		vote=Vote.all.where("email='#{email}' and flag=''")
+		if vote==nil
+			@result='您的Email已領過票，或Email錯誤或未登錄'
+		else
 
 			if email.length()>5
 				temp=email[0..4].to_s()
@@ -35,7 +35,9 @@ class WelcomeController < ApplicationController
 			}
 
 			r=Serial.all.where("number='#{s}'")
-			if r==0
+			#p 'jaiejfio;awjfiwjio'
+			#p r.count
+			if r.count==0
 				serial=Serial.new()
 				serial.number=@result
 				serial.save()
@@ -43,7 +45,7 @@ class WelcomeController < ApplicationController
 			else
 				@result='您的Email已領過票，或Email錯誤或未登錄'
 			end
-		#end
+		end
 	end
 
 
